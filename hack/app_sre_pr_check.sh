57d9d499baa28012b157ee4f49d434653eb9319d
Reetika Vyas <revyas@redhat.com>
2021-08-23 09:50:02 +0530

    validate metadata

--- hack/app_sre_pr_check.sh
+++ hack/app_sre_pr_check.sh
@@ -3,5 +3,9 @@
 set -ex
 
 cd $(dirname $0)/..
+#validate json schema
+pip install jsonschema
+
+find . -name 'metadata.yaml' -exec jsonschema --instance {} hack/medata.schema.json \;
 
 make IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-app-sre} build
