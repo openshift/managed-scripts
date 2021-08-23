9c4bae3e1aff32444f32524f888b95c1931ed21b
Reetika Vyas <revyas@redhat.com>
2021-08-23 09:50:02 +0530

    validate json schema

--- hack/app_sre_pr_check.sh
+++ hack/app_sre_pr_check.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-set -ex
+#set -ex
 
 cd $(dirname $0)/..
 
-make IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-app-sre} build
+#convert all metadata.yaml to json first
+yfiles=$(find . -name 'metadata.yaml')
+for f in $yfiles
+do
+   echo "convert $f to json"
+   python3 hack/yamltojson.py $f
+done
+#Verify the metadata.json are valide
+jfiles=$(find . -name 'metadata.json')
+for f in $jfiles
+do
+   echo "validating the jsonschema for $f"
+   if ! jsonschema --instance $f ./hack/metadata.schema.json; then
+     echo "validating failed: $f"
+   else
+     echo "validating succeed"
+   fi
+done
+
+#make IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-app-sre} build
