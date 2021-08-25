41a002860f7722af22174f03db05787f02f16883
Reetika Vyas <revyas@redhat.com>
2021-08-26 00:07:56 +0530

    Added convertor

--- hack/app_sre_pr_check.sh
+++ hack/app_sre_pr_check.sh
@@ -3,12 +3,12 @@
 set -ex
 
 cd $(dirname $0)/..
-pwd
+
 #validate json schema
 
-#pip install --user jsonschema
-#pip install --user  pyyaml
-python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' scripts/SREP/example/metadata.yaml > scripts/SREP/example/metadata.json
+pip install --user jsonschema
+pip install --user  pyyaml
+python3 yamltojson.py
 find . -name 'metadata.json' -exec jsonschema --instance {} hack/metadata.schema.json \;
 
-#make IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-app-sre} build
+make IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-app-sre} build
---
+++ hack/yamltojson.py
@@ -0,0 +1,8 @@
+import yaml
+import json
+import os
+#path = os.getcwd()
+print(path)
+with open("./../scripts/SREP/example/metadata.yaml", 'r') as yaml_in, open("./../scripts/SREP/example/metadata.json", "w") as json_out:
+    yaml_object = yaml.safe_load(yaml_in) # yaml_object will be a list or a dict
+    json.dump(yaml_object, json_out)
