0301b7c9e8fbfed3240b3304ca7eee43a3b55419
Reetika Vyas <revyas@redhat.com>
2021-08-26 00:14:00 +0530

    path changed in pr_check script

--- hack/app_sre_pr_check.sh
+++ hack/app_sre_pr_check.sh
@@ -4,11 +4,13 @@ set -ex
 
 cd $(dirname $0)/..
 
-#validate json schema
+ #validate json schema
 
 pip install --user jsonschema
 pip install --user  pyyaml
-python3 yamltojson.py
+pip install --user ruamel.yaml
+pwd
+python3 hack/build/yamltojson.py
 find . -name 'metadata.json' -exec jsonschema --instance {} hack/metadata.schema.json \;
 
 make IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-app-sre} build
