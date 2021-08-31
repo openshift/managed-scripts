8a5f20f43501b2f122d2f4bfa0c345db56807fc8
Reetika Vyas <revyas@redhat.com>
2021-08-31 11:26:27 +0530

    added reviewed changes

--- hack/app_sre_pr_check.sh
+++ hack/app_sre_pr_check.sh
@@ -5,7 +5,7 @@ set -ex
 cd $(dirname $0)/..
 
 #convert yaml to json
-find . -name 'metadata.yaml' -exec python3 hack/yamltojson.py scripts/SREP/example/metadata.yaml  {} \;
+find . -name 'metadata.yaml' -exec python3 hack/yamltojson.py {} \;
 
 #validate json schema
 find . -name 'file:///metadata.json' -exec jsonschema --instance {} file:///hack/metadata.schema.json \;
