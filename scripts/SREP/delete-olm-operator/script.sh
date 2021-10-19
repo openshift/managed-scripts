552a3ed139500d2aaffb3d0f6f6418b1517ad740
Ron Green <11993626+georgettica@users.noreply.github.com>
2021-10-19 10:55:09 +0300

    Update script.sh

--- scripts/SREP/delete-olm-operator/script.sh
+++ scripts/SREP/delete-olm-operator/script.sh
@@ -11,23 +11,23 @@ fi
 
 "$DELETE" || echo "Not going to delete resources"
 
-SUBSCRIPTION=$(oc get sub -n "${NAMESPACE}" -o jsonpath='{.items[*].metadata.name}')
-CATALOG_SOURCE=$(oc get catalogsource -n "${NAMESPACE}" -o jsonpath='{.items[*].metadata.name}')
-OPERATOR_GROUP=$(oc get og -n "${NAMESPACE}" -o jsonpath='{.items[*].metadata.name}')
+SUBSCRIPTION=$(oc get subscriptions.operators.coreos.com -n "${NAMESPACE}" -o jsonpath='{.items[*].metadata.name}')
+CATALOG_SOURCE=$(oc get catalogsources.operators.coreos.com -n "${NAMESPACE}" -o jsonpath='{.items[*].metadata.name}')
+OPERATOR_GROUP=$(oc get operatorgroups.operators.coreos.com -n "${NAMESPACE}" -o jsonpath='{.items[*].metadata.name}')
 
 
 if $DELETE; then
   if [[ -n "${SUBSCRIPTION}" ]]; then
     echo "Deleting Subscription: ${SUBSCRIPTION}"
-    oc delete sub "${SUBSCRIPTION}" -n "${NAMESPACE}"
+    oc delete subscriptions.operators.coreos.com "${SUBSCRIPTION}" -n "${NAMESPACE}"
   fi
   if [[ -n "${CATALOG_SOURCE}" ]]; then
     echo "Deleting CatalogSource: ${CATALOG_SOURCE}"
-    oc delete catalogsource "${CATALOG_SOURCE}" -n "${NAMESPACE}" 
+    oc delete catalogsources.operators.coreos.com "${CATALOG_SOURCE}" -n "${NAMESPACE}" 
   fi
   if [[ -n "${OPERATOR_GROUP}" ]]; then
     echo "Deleting OperatorGroup: ${OPERATOR_GROUP}"
-    oc delete og "${OPERATOR_GROUP}" -n "${NAMESPACE}" 
+    oc delete operatorgroups.operators.coreos.com "${OPERATOR_GROUP}" -n "${NAMESPACE}" 
   fi
 else
   echo "Will delete Subscription: ${SUBSCRIPTION}"
@@ -36,11 +36,11 @@ else
 fi
 
 $DELETE || echo "Will delete CSVs:"
-for csv in $(oc get csv -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
+for csv in $(oc get clusterserviceversions.operators.coreos.com -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
 do
   if $DELETE; then
     echo "Deleting CSV: ${csv}"
-    oc delete csv "${csv}" -n "${NAMESPACE}"
+    oc delete clusterserviceversions.operators.coreos.com "${csv}" -n "${NAMESPACE}"
   else
     echo -e "\t${csv}"
   fi
