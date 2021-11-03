bf660e9cb72bc166e0e7340bf38a798ffc7489b6
Siu Wa Wu <siwu@redhat.com>
2021-11-03 21:56:36 +0800

    fix test

--- scripts/SREP/node-logs/script.sh
+++ scripts/SREP/node-logs/script.sh
@@ -7,17 +7,17 @@ set -o pipefail
 ## validate input
 if [[ -z "$node" ]]
 then
-    echo 'Variable node cannot be blank please try again'
+    echo 'Variable node cannot be blank'
     exit 1
 fi
 
 if ! [[ "$node" =~ ^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$ ]]
 then
-    echo "The node name must be valid hostname"
+    echo "The node name must be a valid hostname"
     exit 1
 fi
 
 ## get the journal of that node
-oc adm node-logs $node
+oc adm node-logs "$node"
 
 exit 0
