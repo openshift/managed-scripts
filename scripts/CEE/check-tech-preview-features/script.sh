df8e4a13646bd4bc1e93f4359997b5dbc6fc9619
Supreeth Basabattini <sbasabat@redhat.com>
2022-01-21 12:04:36 +0530

    OSD-9511: Enable CEE to identify if customer is running any tech preview feature

---
+++ scripts/CEE/check-tech-preview-features/script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# This script checks if any tech preview features are enabled through feature sets.
+
+set -e
+
+# Set of default enabled feature gates; common for OSD clusters.
+DEFAULT_FEATURE_GATES=(
+"APIPriorityAndFairness=true"
+"RotateKubeletServerCertificate=true"
+"SupportPodPidsLimit=true"
+"NodeDisruptionExclusion=true"
+"ServiceNodeExclusion=true"
+"DownwardAPIHugePages=true"
+"LegacyNodeRoleBehavior=false"
+)
+
+getFeatureSet() {
+    oc get featuregate cluster -o jsonpath='{.spec.featureSet}'
+}
+
+getFeatureGates() {
+    oc get kubeapiserver cluster -o json | jq -c '.spec.observedConfig.apiServerArguments["feature-gates"]' | tr '[]",' ' '
+}
+
+getAdditionalFeatureGates() {
+a=("$@")
+for i in ${a[@]}
+	do
+		if [[ ! ${DEFAULT_FEATURE_GATES[*]}  =~ ${i} ]]; then
+			echo ${i}
+		fi
+	done
+}
+
+feature_set=$(getFeatureSet)
+
+if [ -z "${feature_set}" ]; then
+	echo "-> No feature sets enabled."
+else
+	echo -e "-> '${feature_set}' feature set enabled."
+	echo "-> Additional feature gates enabled:"
+    feature_gates=$(getFeatureGates)
+    getAdditionalFeatureGates ${feature_gates}
+fi
