02ad8afc4a8d7a3fbd227c7f8a3fe7ee2b8f5a27
R-Lawton <rlawton@redhat.com>
2022-03-01 11:10:53 +0000

    adding double quotes

--- scripts/CEE/get-rhosak-operators/get-rhosak-operators.sh
+++ scripts/CEE/get-rhosak-operators/get-rhosak-operators.sh
@@ -6,29 +6,29 @@ function create_directory(){
     mkdir "$directory"
     sinceFlag=()
         if [[ -v since ]]; then
-            sinceFlag+=("--since=${since}")
+            sinceFlag+=("--since=$since")
         elif [[ -v since_time ]]; then
-            sinceFlag+=("--since-time=${since_time}")
+            sinceFlag+=("--since-time=$since_time")
         fi
 }
 
 function fleetshard(){
-    oc adm inspect ns/redhat-kas-fleetshard-operator $sinceFlag --dest-dir $directory > "$directory"/rhosak.log 2>&1
+    oc adm inspect ns/redhat-kas-fleetshard-operator "${sinceFlag[@]}" --dest-dir "$directory" > "$directory"/rhosak.log 2>&1
 }
 function observability(){
-    oc adm inspect ns/managed-application-services-observability $sinceFlag --dest-dir $directory > "$directory"/rhosak.log 2>&1
+    oc adm inspect ns/managed-application-services-observability "${sinceFlag[@]}" --dest-dir "$directory" > "$directory"/rhosak.log 2>&1
 }
 function strimzi(){
-    oc adm inspect  ns/redhat-managed-kafka-operator --dest-dir $directory > "$directory"/rhosak.log 2>&1
+    oc adm inspect  ns/redhat-managed-kafka-operator "${sinceFlag[@]}" --dest-dir "$directory" > "$directory"/rhosak.log 2>&1
 }
 function agent(){
     oc get -o yaml managedkafkaagents -n redhat-kas-fleetshard-operator > "$directory"/managed-kafka-agents.txt 2>&1
 }
 function clean(){
-    find "${directory}" -type f -name "rhosak.log" -delete
-    find "${directory}" -type f -name secrets.yaml -delete
-    find "${directory}" -name "*.txt" -exec sed -i '/accessToken:/d' {} \;
-    tar -zcf  $directory.tar.gz ./"$directory"
+    find "$directory" -type f -name "rhosak.log" -delete
+    find "$directory" -type f -name secrets.yaml -delete
+    find "$directory" -name "*.txt" -exec sed -i '/accessToken:/d' {} \;
+    tar -zcf  "$directory".tar.gz ./"$directory"
 }
 
 create_directory
