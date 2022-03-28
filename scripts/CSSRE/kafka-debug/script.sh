a6ff753ff735451e6f7f2a6cb2d5704dd561cea6
npecka <zangoose107@gmail.com>
2022-03-28 12:26:14 -0400

    Modified file based on pr-check
    Signed-off-by: npecka <zangoose107@gmail.com>

--- scripts/CSSRE/kafka-debug/script.sh
+++ scripts/CSSRE/kafka-debug/script.sh
@@ -30,13 +30,13 @@ function main(){
 
   podFailCount=0
   
-  printf "_______________________________________________________________  \n"
-  printf " Kafka Resource Status\n" 
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
+  echo " Kafka Resource Status" 
+  echo "_______________________________________________________________"
 
-  printf "_______________________________________________________________  \n"
-  printf " Deployment/Statefulset Status\n" 
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
+  echo " Deployment/Statefulset Status" 
+  echo "_______________________________________________________________"
  
   deploymentCount=$(oc get deployments -n "${resource_namespace}" | tail -n +2 | wc -l | sed 's/^ *//g')
   echo "Deployment(s) $deploymentCount" > kafka-deployment-statefulsets.txt
@@ -44,80 +44,80 @@ function main(){
   statefulsetCount=$(oc get statefulsets -n "${resource_namespace}" | tail -n +2 | wc -l | sed 's/^ *//g')
   echo "Statefulset(s) $statefulsetCount" >> kafka-deployment-statefulsets.txt
 
-  cat kafka-deployment-statefulsets.txt | column -t
+  cat < kafka-deployment-statefulsets.txt | column -t
 
-  printf "_______________________________________________________________  \n"
-  printf " Component Status\n" 
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
+  echo " Component Status" 
+  echo "_______________________________________________________________"
 
   
   kafkaBrokerRequired=$(oc get statefulset "${resource_cluster_name}"-kafka -n "${resource_namespace}" -ojson | jq '.spec.replicas')
   kafkaBrokerCount=$(oc get statefulset "${resource_cluster_name}"-kafka -n "${resource_namespace}" -ojson | jq '.status.readyReplicas')  
   echo "Kafka-Brokers $kafkaBrokerCount/$kafkaBrokerRequired ok" > kafka-pods.txt
-  if [ $kafkaBrokerRequired -ne $kafkaBrokerCount ]; then
-    (($podFailCount=$podFailCount+1))
+  if [ "$kafkaBrokerRequired" -ne "$kafkaBrokerCount" ]; then
+    podFailCount=$((podFailCount+1))
   fi
  
   zookeeperBrokerRequired=$(oc get statefulset "${resource_cluster_name}"-zookeeper -n "${resource_namespace}" -ojson | jq '.spec.replicas')
   zookeeperBrokerCount=$(oc get statefulset "${resource_cluster_name}"-zookeeper -n "${resource_namespace}" -ojson | jq '.status.readyReplicas')  
   echo "Zookeeper-Brokers $zookeeperBrokerCount/$zookeeperBrokerRequired ok" >> kafka-pods.txt
-  if [ $zookeeperBrokerRequired -ne $zookeeperBrokerCount ]; then
-    (($podFailCount=$podFailCount+1))
+  if [ "$zookeeperBrokerRequired" -ne "$zookeeperBrokerCount" ]; then
+    podFailCount=$((podFailCount+1))
   fi
 
   adminServerRequired=$(oc get deployment "${resource_cluster_name}"-admin-server -n "${resource_namespace}" -ojson | jq '.spec.replicas')
   adminServerCount=$(oc get deployment "${resource_cluster_name}"-admin-server -n "${resource_namespace}" -ojson | jq '.status.readyReplicas')
   echo "Admin-Server(s) $adminServerCount/$adminServerRequired ok" >> kafka-pods.txt
-  if [ $adminServerRequired -ne $adminServerCount ]; then
-    (($podFailCount=$podFailCount+1))
+  if [ "$adminServerRequired" -ne "$adminServerCount" ]; then
+    podFailCount=$((podFailCount+1))
   fi
 
   canaryRequired=$(oc get deployment "${resource_cluster_name}"-canary -n "${resource_namespace}" -ojson | jq '.spec.replicas')
   canaryCount=$(oc get deployment "${resource_cluster_name}"-canary -n "${resource_namespace}" -ojson | jq '.status.readyReplicas')
   echo "Canary(s) $canaryCount/$canaryRequired ok" >> kafka-pods.txt
   if [ $canaryRequired -ne $canaryCount ]; then
-    (($podFailCount=$podFailCount+1))
+    podFailCount=$((podFailCount+1))
   fi
 
   kafkaExporterRequired=$(oc get deployment "${resource_cluster_name}"-kafka-exporter -n "${resource_namespace}" -ojson | jq '.spec.replicas')
   kafkaExporterCount=$(oc get deployment "${resource_cluster_name}"-kafka-exporter -n "${resource_namespace}" -ojson | jq '.status.readyReplicas')
   echo "Kafka-Exporter(s) $kafkaExporterCount/$kafkaExporterRequired ok" >> kafka-pods.txt
-  if [ $kafkaExporterRequired -ne $kafkaExporterCount ]; then
-    (($podFailCount=$podFailCount+1))
+  if [ "$kafkaExporterRequired" -ne "$kafkaExporterCount" ]; then
+    podFailCount=$((podFailCount+1))
   fi
 
   podCount=$(oc get pods -n "${resource_namespace}" | grep 1/1 | wc -l | sed 's/^ *//g')
   podRequired=$((kafkaBrokerRequired + zookeeperBrokerRequired + adminServerRequired + canaryRequired + kafkaExporterRequired))
   echo "---" >> kafka-pods.txt
   echo "Pod(s)-Running $podCount/$podRequired ok" >> kafka-pods.txt
-  if [ $podCount -ne $podRequired ]; then
+  if [ "$podCount" -ne "$podRequired" ]; then
     podFailCount=$((podFailCount+1))
   fi
 
-  cat kafka-pods.txt | column -t
+  cat < kafka-pods.txt | column -t
 
-  printf "_______________________________________________________________  \n"
-  printf " Gathering Logs for Unfiltered Kafka Broker Data\n" 
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
+  echo " Gathering Logs for Unfiltered Kafka Broker Data" 
+  echo "_______________________________________________________________"
 
   if [ $kafkaBrokerCount -ne 0 ]; then
     for (( i=0; i<=$kafkaBrokerCount-1; i++))
     do
         oc logs "${resource_cluster_name}"-kafka-"$i" -n "${resource_namespace}" --tail 1000 | egrep -v '.*INFO.*$|.*TRACE.*$' > kafka-"$i".txt
-        printf "kafka-"$i".txt created\n"
+        echo "kafka-"$i".txt created"
     done
   fi
 
-  printf "_______________________________________________________________  \n"
-  printf " Cluster Events\n" 
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
+  echo " Cluster Events" 
+  echo "_______________________________________________________________"
 
   oc get events -n "${resource_namespace}" | egrep -v '.*Normal.*$' > kafka-events.txt
-  printf "kafka-events.txt created\n"
+  echo "kafka-events.txt created"
 
-  printf "_______________________________________________________________  \n"
-  printf " Networking Status\n" 
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
+  echo " Networking Status" 
+  echo "_______________________________________________________________"
  
   serviceCount=$(oc get services -n "${resource_namespace}" | tail -n +2 | wc -l | sed 's/^ *//g')
   echo "Service(s) $serviceCount" > kafka-services.txt
@@ -125,43 +125,43 @@ function main(){
   routeCount=$(oc get routes -n "${resource_namespace}" | tail -n +2 | wc -l | sed 's/^ *//g')
   echo "Route(s) $routeCount" >> kafka-services.txt
 
-  cat kafka-services.txt | column -t
+  cat < kafka-services.txt | column -t
 
-  printf "_______________________________________________________________  \n"
-  printf " Checking Network Status for Availability\n" 
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
+  echo " Checking Network Status for Availability" 
+  echo "_______________________________________________________________"
  
-  oc run -q --env brokerCount=$kafkaBrokerCount $resource_cluster_name --image=registry.redhat.io/rhel7/rhel-tools -i --restart=Never --rm -- <test-endpoint.sh 2>/dev/null | column -t
+  oc run -q --env brokerCount="$kafkaBrokerCount" "$resource_cluster_name" --image=registry.redhat.io/rhel7/rhel-tools -i --restart=Never --rm -- <test-endpoint.sh 2>/dev/null | column -t
 
-  printf "_______________________________________________________________  \n"
-  printf " Storage Status\n" 
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
+  echo " Storage Status" 
+  echo "_______________________________________________________________"
  
-  pvCount=$(oc get pv | grep "${resource_namespace}" | grep Bound | wc -l | sed 's/^ *//g')
+  pvCount=$(oc get pv | grep "${resource_namespace}" | grep -c Bound | sed 's/^ *//g')
   echo "Persistent-Volume(s) $pvCount bound" > kafka-volumes.txt
 
-  pvcCount=$(oc get pvc -n "${resource_namespace}" | grep Bound | wc -l | sed 's/^ *//g')
+  pvcCount=$(oc get pvc -n "${resource_namespace}" | grep -c Bound | sed 's/^ *//g')
   echo "Persistent-Volume-Count $pvcCount bound" >> kafka-volumes.txt
 
-  cat kafka-volumes.txt | column -t
+  cat < kafka-volumes.txt | column -t
 
-  printf "_______________________________________________________________  \n"
-  printf " Secrets\n" 
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
+  echo " Secrets" 
+  echo "_______________________________________________________________"
  
   secretCount=$(oc get secrets -n "${resource_namespace}" | tail -n +2 | wc -l | sed 's/^ *//g')
-  printf "Secret(s) $secretCount\n"
+  echo "Secret(s) $secretCount"
 
-  printf "_______________________________________________________________  \n"
-  printf " ConfigMaps\n" 
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
+  echo " ConfigMaps" 
+  echo "_______________________________________________________________"
  
   configmapCount=$(oc get configmaps -n "${resource_namespace}" | tail -n +2 | wc -l | sed 's/^ *//g')
-  printf "Configmap(s) $configmapCount\n"
+  echo "Configmap(s) $configmapCount"
 
-  printf "_______________________________________________________________  \n"
-  printf " Kafka Cluster Items in order by AGE\n" 
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
+  echo " Kafka Cluster Items in order by AGE" 
+  echo "_______________________________________________________________"
 
   echo "-------------------------DEPLOYMENTS-----------------------------" > kafka-age.txt
   oc get deployments -n "${resource_namespace}" --sort-by=.metadata.creationTimestamp >> kafka-age.txt
@@ -190,12 +190,12 @@ function main(){
   echo "-------------------------CONFIGMAPS-----------------------------" >> kafka-age.txt
   oc get configmaps -n "${resource_namespace}" --sort-by=.metadata.creationTimestamp >> kafka-age.txt
 
-  printf "kafka-status-age.txt created\n"
+  echo "kafka-status-age.txt created"
 
   totalCount=$((podCount + serviceCount + pvCount + pvcCount + deploymentCount + kafkaBrokerCount + zookeeperBrokerCount))
  
-  printf "_______________________________________________________________  \n"
-  printf " Total Resources check $totalCount\n"
+  echo "_______________________________________________________________"
+  echo " Total Resources check $totalCount"
   echo "Pod(s) $podCount/$podRequired" > kafka-total-resources.txt
   echo "Service(s) $serviceCount" >> kafka-total-resources.txt
   echo "Route(s) $routeCount" >> kafka-total-resources.txt
@@ -204,12 +204,12 @@ function main(){
   echo "Secret(s) $secretCount" >> kafka-total-resources.txt
   echo "Configmap(s) $configmapCount" >> kafka-total-resources.txt
   cat kafka-total-resources.txt | column -t
-  printf "_______________________________________________________________  \n"
+  echo "_______________________________________________________________"
 
   if [[ $podFailCount -ne 0 ]]; then
-    printf "_______________________________________________________________  \n"
-    printf " FAILURES\n"
-    printf "Pod-Failure(s) $podFailCount Total-Pod(s) $podCount/$podRequired\n" | column -t
+    echo "_______________________________________________________________"
+    echo " FAILURES\n"
+    echo "Pod-Failure(s) $podFailCount Total-Pod(s) $podCount/$podRequired" | column -t
   fi
 
 }
--- scripts/CSSRE/kafka-debug/test-endpoint.sh
+++ scripts/CSSRE/kafka-debug/test-endpoint.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 resource_cluster_name=$(cat /etc/hostname)
-bCount=$(($brokerCount-1))
+bCount=$((brokerCount-1))
 test_endpoint() 
 {
-  nc -z -w 2 $1 $2
+  nc -z -w 2 "$1" "$2"
   if [ $? -eq 0 ]; then
     echo -e "$1 $2 ok"
   else
-    echo $1 $2 FAILED
+    echo "$1" "$2" FAILED
   fi
 }
 
-test_endpoint ${resource_cluster_name}-admin-server 8443
-test_endpoint ${resource_cluster_name}-canary 8080
+test_endpoint "${resource_cluster_name}"-admin-server 8443
+test_endpoint "${resource_cluster_name}"-canary 8080
 
 for i in $(seq 0 $bCount)
 do
-  test_endpoint ${resource_cluster_name}-kafka-$i 9094
+  test_endpoint "${resource_cluster_name}"-kafka-$i 9094
 done
 
-test_endpoint ${resource_cluster_name}-kafka-bootstrap 9096
-test_endpoint ${resource_cluster_name}-kafka-brokers 9096
-test_endpoint ${resource_cluster_name}-kafka-external-bootstrap 9094
+test_endpoint "${resource_cluster_name}"-kafka-bootstrap 9096
+test_endpoint "${resource_cluster_name}"-kafka-brokers 9096
+test_endpoint "${resource_cluster_name}"-kafka-external-bootstrap 9094
