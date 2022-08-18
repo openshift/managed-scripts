eef965b95f9359d139a96edc47b44d1e7ec14cde
Rachel Ryan <raryan@redhat.com>
2022-08-18 16:22:36 +0100

    Update describe-kafka-transaction to use services instead of statefulsets

--- scripts/CEE/describe-kafka-transaction/describe-kafka-transaction.sh
+++ scripts/CEE/describe-kafka-transaction/describe-kafka-transaction.sh
@@ -3,10 +3,10 @@ set -o pipefail
 set -o nounset
 
 function main() {
-    local kafkaStatefulSet
-    kafkaStatefulSet=$(oc -n "${resource_namespace}" get statefulset -l app.kubernetes.io/name=kafka -o name)
-
-    oc exec -n "${resource_namespace}" "${kafkaStatefulSet}" -c kafka -- \
+    local service
+    service=$(oc -n "${resource_namespace}" get service  -o name -l "app.kubernetes.io/name=kafka" | grep bootstrap | head -n1)
+    
+    oc exec -n "${resource_namespace}" "${service}" -c kafka -- \
        ./bin/kafka-transactions.sh --bootstrap-server localhost:9096 describe --transactional-id "${transactional_id}"
 }
 
