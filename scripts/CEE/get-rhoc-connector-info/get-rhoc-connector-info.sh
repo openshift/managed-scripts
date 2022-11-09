08c9966fe5caaa641f1e95a6397b6a1f2c993a34
Torsten Mielke <tmielke@redhat.com>
2022-11-09 11:42:30 +0100

    [MGDCTRS-1665] sending collected information to stdout using tar

--- scripts/CEE/get-rhoc-connector-info/get-rhoc-connector-info.sh
+++ scripts/CEE/get-rhoc-connector-info/get-rhoc-connector-info.sh
@@ -8,14 +8,13 @@ mkdir "${outputDir}"
 set -o pipefail
 set -o nounset
 
-echo "Collecting resources for connector ${connector_id}"         
-
+# Collecting resources for connector ${connector_id}
 oc get \
     ManagedConnector,KameletBinding,Integration,IntegrationKit,KafkaConnect,KafkaConnector,Deployment,ReplicaSet,Pod,PodDisruptionBudget,NetworkPolicy,ConfigMap,Service,EndpointSlice \
     -l cos.bf2.org/connector.id="${connector_id}" \
     --all-namespaces \
     -o yaml > "${outputDir}/resources.yaml"
-       
+
 NAMESPACED_NAMES=$(oc get pods --selector=cos.bf2.org/connector.id="${connector_id}" --all-namespaces -o jsonpath='{range .items[*]}{.metadata.namespace}{";"}{.metadata.name}{" "}{end}')
 NAMESPACE=""
 
@@ -24,14 +23,18 @@ for NAMESPACED_NAME in $NAMESPACED_NAMES; do
     POD=$(echo "${NAMESPACED_NAME}"| cut -d ";" -f 2)
 
     if [ -z "${nologs+x}" ]; then
-        echo "Collecting logs for pod ${POD} in namespace ${NAMESPACE}"         
+        # Collecting logs for pod ${POD} in namespace ${NAMESPACE}
         oc logs "${POD}" --namespace "${NAMESPACE}" | sed -e 's/\x1b\[[0-9;]*m//g' > "${outputDir}/log-${NAMESPACE}-${POD}.txt"
     fi
 done
 
-echo "Collecting events for namespace ${NAMESPACE}" 
-
+# Collecting events for namespace ${NAMESPACE}
 oc get events --namespace "${NAMESPACE}" -o yaml > "${outputDir}/events-${NAMESPACE}.yaml"
 
 # collect events in text format as well
 oc get events --namespace "${NAMESPACE}" > "${outputDir}/events-${NAMESPACE}.txt"
+
+# tar up all collected data to stdout
+# users can collect the data by running
+# kubectl -n openshift-backplane-managed-scripts logs <job-id> | tar xzf - 
+tar zcf - ./"${outputDir}"
