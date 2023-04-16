74b8e52a8109748be555aa1a535cf63dd8c158ff
Reetika Vyas <revyas@redhat.com>
2023-04-16 08:32:50 +0530

    Removed shellcheck script

--- scripts/SREP/example/script.sh
+++ scripts/SREP/example/script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Source the logging system
-source ../LIBRARY/liblog.sh
-source ../LIBRARY/libshellcheck.sh
+source ../../LIBRARY/liblog.sh
 
 # Log a message at the INFO level
 log_info "Getting pods in openshift-monitoring namespace"
@@ -13,12 +12,8 @@ oc -n openshift-monitoring get po
 # Log a message at the DEBUG level, with a variable interpolated in the message
 var1="foo"
 log_debug "The value of var1 is ${var1}"
-log_info "Checking script for errors"
-if ! ../LIBRARY/libshellcheck.sh "$0"; then
-log_error "Script contains errors"
-exit 1
-fi
 
-log_info "Script validated successfully"
+log_info "Script executed successfully"
 exit 0
+
 
