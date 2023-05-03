37a90a4d50226af3d5ddd6e12d5e37e5d8e3b55a
Hector Kemp <hkemp@redhat.com>
2023-05-03 12:51:18 +1000

    Add managed job library support

---
+++ scripts/examples/lib-sourcer/script.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# All files in scripts end up at /managed-scripts in the actual docker container
+# i.e.
+# managed-scripts/scripts/examples/lib-sourcer/lib.sh
+# becomes
+source /managed-scripts/lib/examples/lib.sh
+
+echo_import
