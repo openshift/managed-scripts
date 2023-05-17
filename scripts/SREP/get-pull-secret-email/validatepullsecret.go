983bb0f7cd761a701dde492512b5981ce6798fa1
Ben Miller <bemiller@redhat.com>
2023-05-17 13:56:31 -0600

    build success!

--- scripts/SREP/get-pull-secret-email/validatepullsecret.go
+++ scripts/SREP/get-pull-secret-email/validatepullsecret.go
@@ -6,27 +6,27 @@ import (
 
 	v1 "github.com/openshift-online/ocm-sdk-go/accountsmgmt/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/types"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
-func getPullSecretEmail(clusterID string, secret *corev1.Secret, sendServiceLog bool) (string, error, bool) {
+func getPullSecretEmail(secret *corev1.Secret, sendServiceLog bool) (string, error) {
 	dockerConfigJsonBytes, found := secret.Data[".dockerconfigjson"]
 	if !found {
 		// Indicates issue w/ pull-secret, so we can stop evaluating and specify a more direct course of action
 		fmt.Println("Secret does not contain expected key '.dockerconfigjson'.")
-		return "", nil, true
+		return "", nil
 	}
 
 	dockerConfigJson, err := v1.UnmarshalAccessToken(dockerConfigJsonBytes)
 	if err != nil {
-		return "", err, true
+		return "", err
 	}
 
 	cloudOpenshiftAuth, found := dockerConfigJson.Auths()["cloud.openshift.com"]
 	if !found {
-		return "", nil, true
+		return "", nil
 	}
 
 	clusterPullSecretEmail := cloudOpenshiftAuth.Email()
@@ -35,9 +35,9 @@ func getPullSecretEmail(clusterID string, secret *corev1.Secret, sendServiceLog 
 			"Couldn't extract email address from pull secret for cloud.openshift.com",
 			"This can mean the pull secret is misconfigured. Please verify the pull secret manually:",
 			"  oc get secret -n openshift-config pull-secret -o json | jq -r '.data[\".dockerconfigjson\"]' | base64 -d")
-		return "", nil, true
+		return "", nil
 	}
-	return clusterPullSecretEmail, nil, false
+	return clusterPullSecretEmail, nil
 }
 
 func main() {
@@ -52,14 +52,23 @@ func main() {
 	}
 
 	secret := &corev1.Secret{}
-	err := clientset.Get(context.TODO(), types.NamespacedName{Namespace: "openshift-config", Name: "pull-secret"}, secret)
+	secretsClient := clientset.CoreV1().Secrets("openshift-config")
+	secret, err = secretsClient.Get(context.TODO(), "pull-secret", metaV1.GetOptions{})
 	if err != nil {
-		return err
+		panic(err.Error())
+	}
+	//clientset.Get(context.TODO(), types.NamespacedName{Namespace: "openshift-config", Name: "pull-secret"}, secret)
+	if err != nil {
+		panic(err.Error())
 	}
 
-	clusterPullSecretEmail, err, done := getPullSecretEmail(clusterID, secret, true)
-	if done {
-		return err
+	clusterPullSecretEmail, err := getPullSecretEmail(secret, true)
+	if err != nil {
+		panic(err.Error())
 	}
+
+	println("Cluster Pull Secret Email: " + clusterPullSecretEmail)
+
+	return
 
 }
