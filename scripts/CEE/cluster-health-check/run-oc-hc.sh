f7aeae3932586614cad9d499f767d01b658e65ac
Givaldo Lins <gilins@redhat.com>
2023-07-06 10:07:58 -0600

    added quotes to the URL used by curl

--- scripts/CEE/cluster-health-check/run-oc-hc.sh
+++ scripts/CEE/cluster-health-check/run-oc-hc.sh
@@ -3,7 +3,7 @@
 set -eou pipefail
 
 # Download latest version and move it to /usr/local/bin
-curl -sL $(curl -s https://api.github.com/repos/givaldolins/openshift-cluster-health-check/releases/latest | grep browser_download_url | cut -d\" -f4 | grep -E 'linux-amd64.tar.gz$') | tar zx
+curl -sL "$(curl -s https://api.github.com/repos/givaldolins/openshift-cluster-health-check/releases/latest | grep browser_download_url | cut -d\" -f4 | grep -E 'linux-amd64.tar.gz$')" | tar zx
 mv oc-hc /usr/local/bin
 
 # Run check
