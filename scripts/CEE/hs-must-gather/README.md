12aa5004d67115a7fd73820c56fa6b1105341eeb
Tafhim Ul Islam <taislam@redhat.com>
2023-08-10 03:08:43 +1000

    OSD-17639 - Add full hypershift dump

--- scripts/CEE/hs-must-gather/README.md
+++ scripts/CEE/hs-must-gather/README.md
@@ -1,17 +1,14 @@
 # Hypershift mustgather
-
-This script run must gather commands in both the hosted and management clusters. 
+This script runs hypershift dump command to fetch logs from management cluster HCP namespace and hosted cluster. 
 
 
 ## Usage
 
 Parameters:
 - CLUSTER_NAME: hosted cluster name.
-- DUMP_DIR: Must gather dump directory path.
 
 ```bash
-ocm backplane managedjob create CEE/hs-must-gather -p CLUSTER_NAME=my-hs-cluster-name -p DUMP_DIR=/path/for-dump/
-
+ocm backplane managedjob create CEE/hs-must-gather -p CLUSTER_NAME=my-hs-cluster-name 
 ```
 
 
--- scripts/CEE/hs-must-gather/script.sh
+++ scripts/CEE/hs-must-gather/script.sh
@@ -4,46 +4,30 @@ set -e
 set -o nounset
 set -o pipefail
 
-#define vars 
-HS_HCP_MG_DIR="$DUMP_DIR/managment-cluster"
-HS_DP_MG_DIR="$DUMP_DIR/hs-cluster"
-HS_BINARY_PATH=" /usr/local/bin/hypershift"
+# Define expected values 
+HS_BINARY_PATH="/usr/local/bin/hypershift"
 
- # check hypershift binary
-function hasHipershiftBinary {
-    echo "Checking hypershift binary available in $HS_BINARY_PATH" 
-    if test -f "$HS_BINARY_PATH";
-     then
-        return 1
-     else
-        return 0
-    fi
-   
-}
-
-# Init must gather for Hosted controll plane
-function initMustGatherforHCP {
-    echo "Init must gather for management cluster namespace $HS_NAMESPACE, and data will saved to $HS_HCP_MG_DIR" 
-
-    return 1
-}
-
-# Init must gather for hosted cluster
-function initMustGatherforHC {
-    echo "Init must gather for hosted cluster $CLUSTER_NAME, and data will saved to $HS_DP_MG_DIR" 
-
-    return 1
-}
-
-# Get 
-if hasHipershiftBinary "$1" ; then
-    if initMustGatherforMC "$1" ; then
-        initMustGatherforHC
-    fi
-
-    
-else
-    echo "hypershift binary not available in $HS_BINARY_PATH path" 
+# Check if hypershift binary is in place 
+if [ ! -f "$HS_BINARY_PATH" ]; then
+  echo "Hypershift binary not available in $HS_BINARY_PATH path" 
+  exit 1
 fi
 
+# Sanity check for multiple namespaces matching label
+HCP_NS_LIST_LENGTH=$(oc get namespace -l api.openshift.com/name="$CLUSTER_NAME" -o json | jq -r '.items | length')
+if [ "$HCP_NS_LIST_LENGTH" -ne 1 ]; then
+  echo "Number of HCP namespaces matching $CLUSTER_NAME must be 1, $HCP_NS_LIST_LENGTH found"
+  exit 1
+fi
+
+# Fetch the target HCP namespace for the cluster name
+HCP_NS=$(oc get namespace -l api.openshift.com/name="$CLUSTER_NAME" -o json | jq -r '.items[0].metadata.labels["kubernetes.io/metadata.name"]')
+
+echo "Executing hypershift dump on $HCP_NS for cluster $CLUSTER_NAME"
+hypershift dump cluster --dump-guest-cluster --namespace "$HCP_NS" --name "$CLUSTER_NAME" --artifact-dir "$CLUSTER_NAME"
+
+echo "Hypershift dump has been saved in $PWD/$CLUSTER_NAME"
+ls -alh "$PWD"/"$CLUSTER_NAME"
+
+# End
 exit 0
