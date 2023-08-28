6af121764a74a00ce2bd71def69043d4ad4b39e7
Tafhim <tafhim.ul.islam@gmail.com>
2023-08-28 23:23:34 +1000

    OSD-17639 - Address PR comments

--- scripts/CEE/hs-must-gather/script.sh
+++ scripts/CEE/hs-must-gather/script.sh
@@ -9,25 +9,25 @@ HS_BINARY_PATH="/usr/local/bin/hypershift"
 
 # Check if hypershift binary is in place 
 if [ ! -f "$HS_BINARY_PATH" ]; then
-  echo "Hypershift binary not available in $HS_BINARY_PATH path" 
+  echo "HyperShift binary not found in $HS_BINARY_PATH path" 
   exit 1
 fi
 
 # Sanity check for multiple namespaces matching label
-HCP_NS_LIST_LENGTH=$(oc get namespace -l api.openshift.com/name="$CLUSTER_NAME" -o json | jq -r '.items | length')
+HCP_NS_LIST_LENGTH=$(oc get namespace -l api.openshift.com/id="$CLUSTER_ID" -o json | jq -r '.items | length')
 if [ "$HCP_NS_LIST_LENGTH" -ne 1 ]; then
-  echo "Number of HCP namespaces matching $CLUSTER_NAME must be 1, $HCP_NS_LIST_LENGTH found"
+  echo "Number of HCP namespaces matching $CLUSTER_ID must be 1, $HCP_NS_LIST_LENGTH found"
   exit 1
 fi
 
 # Fetch the target HCP namespace for the cluster name
-HCP_NS=$(oc get namespace -l api.openshift.com/name="$CLUSTER_NAME" -o json | jq -r '.items[0].metadata.labels["kubernetes.io/metadata.name"]')
+HCP_NS=$(oc get namespace -l api.openshift.com/id="$CLUSTER_ID" -o json | jq -r '.items[0].metadata.labels["kubernetes.io/metadata.name"]')
 
-echo "Executing hypershift dump on $HCP_NS for cluster $CLUSTER_NAME"
-hypershift dump cluster --dump-guest-cluster --namespace "$HCP_NS" --name "$CLUSTER_NAME" --artifact-dir "$CLUSTER_NAME"
+echo "Executing hypershift dump on $HCP_NS for cluster $CLUSTER_ID"
+#hypershift dump cluster --dump-guest-cluster --namespace "$HCP_NS" --name "$CLUSTER_ID" --artifact-dir "$CLUSTER_ID"
 
-echo "Hypershift dump has been saved in $PWD/$CLUSTER_NAME"
-ls -alh "$PWD"/"$CLUSTER_NAME"
+echo "Hypershift dump has been saved in $PWD/$CLUSTER_ID"
+ls -alh "$PWD"/"$CLUSTER_ID"
 
 # End
 exit 0
