ace473d8da19eeab0e2c30763076b92826c1809f
Tafhim Ul Islam <taislam@taislam-thinkpadt14sgen2i.remote.csb>
2023-09-05 18:01:38 +1000

    OSD-17639 - Update comment and prevent creation of archive

--- scripts/CEE/hs-must-gather/script.sh
+++ scripts/CEE/hs-must-gather/script.sh
@@ -13,7 +13,7 @@ if [ ! -f "$HS_BINARY_PATH" ]; then
   exit 1
 fi
 
-# Sanity check for multiple namespaces matching label
+# Sanity check to make sure desired cluster namespace exists
 HCP_NS_LIST_LENGTH=$(oc get namespace -l api.openshift.com/id="$CLUSTER_ID" -o json | jq -r '.items | length')
 if [ "$HCP_NS_LIST_LENGTH" -ne 1 ]; then
   echo "Number of HCP namespaces matching $CLUSTER_ID must be 1, $HCP_NS_LIST_LENGTH found"
@@ -24,7 +24,7 @@ fi
 HCP_NS=$(oc get namespace -l api.openshift.com/id="$CLUSTER_ID" -o json | jq -r '.items[0].metadata.labels["kubernetes.io/metadata.name"]')
 
 echo "Executing hypershift dump on $HCP_NS for cluster $CLUSTER_ID"
-hypershift dump cluster --dump-guest-cluster --namespace "$HCP_NS" --name "$CLUSTER_ID" --artifact-dir "$CLUSTER_ID"
+hypershift dump cluster --dump-guest-cluster --namespace "$HCP_NS" --name "$CLUSTER_ID" --artifact-dir "$CLUSTER_ID" --archive-dump=false
 
 echo "Hypershift dump has been saved in $PWD/$CLUSTER_ID"
 ls -alh "$PWD"/"$CLUSTER_ID"
