b5341a1acf87f7cb6958bb7b03910de9cf755596
Hevellyn <hevs159@outlook.com>
2023-11-14 15:12:41 +0100

    replaced single quote to double quotes

--- scripts/SREP/get-sosreport/script.sh
+++ scripts/SREP/get-sosreport/script.sh
@@ -7,7 +7,7 @@ set -o pipefail
 ## validate input
 if [[ -z "$NODE" ]]
 then
-    echo 'Variable node cannot be blank'
+    echo "Variable node cannot be blank"
     exit 1
 fi
 
@@ -37,21 +37,21 @@ generate_sosreport() {
 # 1st Debug session - Generate the sosreport and keep it in the host
 
     echo " ==== Generating SOSREPORT ===="
-    oc -n default debug node/"${NODE}" -- sh -c 'chroot /host toolbox sos report -k crio.all=on -k crio.logs=on --batch'
+    oc -n default debug node/"${NODE}" -- sh -c "chroot /host toolbox sos report -k crio.all=on -k crio.logs=on --batch"
 
     return 0
 }
 
 validate_file() {
     echo "==== Check if the sosreport is created inside the node ===="
-    oc -n default debug node/"${NODE}" -- bash -c 'ls -l /host/var/tmp/*.tar.xz' | tee;
+    oc -n default debug node/"${NODE}" -- bash -c "ls -l /host/var/tmp/*.tar.xz" | tee;
 
     return 0
 }
 
 copy_sosreport() {
 # 2nd Debug session - Fetch sosreport .tar.xz file and save inside the container volume.
-    oc -n default debug node/"${NODE}" -- bash -c 'cat $(ls -tA /host/var/tmp/sosreport-$HOSTNAME-*.tar.xz | head -1)' > ${DUMP_DIR}/sosreport-${NODE}.tar.xz ;
+    oc -n default debug node/"${NODE}" -- bash -c "cat $(ls -tA /host/var/tmp/sosreport-$HOSTNAME-*.tar.xz | head -1)" > ${DUMP_DIR}/sosreport-${NODE}.tar.xz ;
 
     echo "==== Check if file exists inside container ===="
 
@@ -66,7 +66,7 @@ delete_sosreport_from_node() {
 # Deleting any sosreport from the /host/var/tmp inside node
 
     echo "==== Deleting file from the node ===="
-    oc -n default debug node/"${NODE}" -- sh -c 'rm /host/var/tmp/sosreport-$HOSTNAME-* && ls -l /host/var/tmp/'
+    oc -n default debug node/"${NODE}" -- sh -c "rm /host/var/tmp/sosreport-$HOSTNAME-* && ls -l /host/var/tmp/"
 
     echo "==== SOSREPORT file should be now not showing in the list ===="
 
