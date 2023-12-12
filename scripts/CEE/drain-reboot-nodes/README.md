35e5bb2f56bc114921cee88142de9abbbba56227
Fabio Aldana <97414315+faldanarh@users.noreply.github.com>
2023-12-12 11:28:56 +1000

    OSD-19453: Fixed the reboot function and metadata.

--- scripts/CEE/drain-reboot-nodes/README.md
+++ scripts/CEE/drain-reboot-nodes/README.md
@@ -2,7 +2,15 @@
 
 ## Description
 
-This script is responsible for draining and rebooting nodes (workers only)
+This script is responsible for draining and rebooting nodes (workers only).
+
+## Steps It Takes
+
+1. Check if the provided node is a Worker node;
+2. If Yes, it starts draining the node with the option to bypass any PDB which may block the process;
+3. After draining, it reboots the node;
+4. There is a function which checks the node reboot. A timeout of 300 seconds has been implemented to stop the process in case any issue is faced during the reboot process;
+5. Once the node is rebooted, it will stay as "Ready,SchedulingDisabled". It is required, after that, to run the managed script to uncordon the node [cordon-uncordon-nodes](https://github.com/openshift/managed-scripts/tree/main/scripts/CEE/cordon-uncordon-nodes).
 
 ## Usage
 
--- scripts/CEE/drain-reboot-nodes/script.sh
+++ scripts/CEE/drain-reboot-nodes/script.sh
@@ -9,7 +9,7 @@ set -e
 set -o nounset
 set -o pipefail
 
-CURRENTDATE=`date +"%Y-%m-%d %T"`
+CURRENTDATE=$(date +"%Y-%m-%d %T")
 
 if [[ -z "${WORKER}" ]]; then
     echo "[Error] Variable WORKER cannot be blank."
@@ -63,28 +63,32 @@ drain_worker(){
 reboot_worker(){
 
     echo "Rebooting node \"${WORKER}\"..."
-    oc -n default debug node/"${WORKER}" -- chroot /host reboot &
-    sleep 5
+
+    cat <<EOF | oc -n default debug node/$WORKER
+    chroot /host
+    reboot
+EOF
 
 }
 
 check_reboot(){
 
+    echo
     echo "Checking node reboot..."
 
     rebootCount=0
     rebootStatus=0
     t0=$(date '+%s')
     timeout_secs=300
-    
-    while [[ $(expr ${timeout_secs} + ${t0} - "$(date '+%s')") -gt 0 ]]; do
+
+    while [[ $(("$timeout_secs" + "$t0" - "$(date '+%s')")) -gt 0 ]]; do
         nodeStatus=$(oc get nodes "${WORKER}" -ojsonpath='{.status.conditions[3].status}{"\n"}')
-            if [ $nodeStatus != "True" ] ; then 
+            if [ "${nodeStatus}" != "True" ] ; then 
                 rebootCount=1
                 echo "Rebooting..."
                 sleep 2
             else
-                if [ $rebootCount -eq 1 ] ; then
+                if [ "${rebootCount}" -eq 1 ] ; then
                     rebootStatus=1
                     echo "Node Rebooted!"
                     break
@@ -92,7 +96,7 @@ check_reboot(){
             fi
     done
     
-    if [ $rebootStatus -eq 0 ] ; then
+    if [ "${rebootStatus}" -eq 0 ] ; then
         echo "[Error] Reboot timeout..."
         exit 1
     fi
