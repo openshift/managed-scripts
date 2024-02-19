afcd691fc601aef8294b2367a368ce7abb4f54df
maupadhy <maupadhy@redhat.com>
2024-02-19 21:21:28 +0530

    changes in script

--- scripts/CEE/kubelet-restart/README.md
+++ scripts/CEE/kubelet-restart/README.md
@@ -1,6 +1,6 @@
-# Restart Kubelet on Master Node
+# Restart Kubelet on Node
 
-This will script will restart kubelet on Master Node. Scenarios where a restart might be required:
+This will script will restart kubelet on Node. Scenarios where a restart might be required:
 - OpenShift related pod stuck in containerUnknown Status
 - Certain bugs require container restart on Master Nodes. [Eg. https://access.redhat.com/solutions/6976343 ] 
 
@@ -8,6 +8,6 @@ This will script will restart kubelet on Master Node. Scenarios where a restart 
 
 Example:
 ```
-ocm backplane managedjob create CEE/restart-kubelet -p MASTER="$MASTER_NODE_NAME"
+ocm backplane managedjob create CEE/restart-kubelet -p NODE="$NODE_NAME"
 ```
 
--- scripts/CEE/kubelet-restart/script.sh
+++ scripts/CEE/kubelet-restart/script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -e
+set -o errexit
 set -o nounset
 set -o pipefail
 
 CURRENTDATE=$(date +"%Y-%m-%d %T")
 
 ## validate input
-if [[ -z "${MASTER}" ]]; then
-    echo "Variable MASTER cannot be blank"
+if [[ -z "${NODE}" ]]; then
+    echo "Variable NODE cannot be blank"
     exit 1
 fi
 
@@ -25,32 +25,18 @@ finish_job(){
 }
 
 
-## Function for MASTER node validation
-check_master(){
-
-    echo "Checking if \"${MASTER}\" is a master node..."
-    
-    if (oc get nodes -l node-role.kubernetes.io/master= -oname | grep "${MASTER}") &> /dev/null; then
-        echo "[OK] \"${MASTER}\" is a master node."
-        echo
-    else
-        echo "[Error] \"${MASTER}\" is not a master node."
-        exit 1
-    fi
-
-}
 
 ## Function for kubelet restart
 restart_kubelet(){
 
-    echo "Restarting Kubelet on \"${MASTER}\"..."
-    cat <<EOF | oc -n default debug node/${MASTER}
+    echo "Restarting Kubelet on \"${NODE}\"..."
+    cat <<EOF | oc -n default debug node/${NODE}
     chroot /host 
     systemctl restart kubelet
 EOF
 
     if [ $? -eq 0 ]; then 
-        echo "[SUCCESS] Kubelet successfully restarted on \"${MASTER}\" ."
+        echo "[SUCCESS] Kubelet successfully restarted on \"${NODE}\" ."
         echo
     else
         echo "[Error] Something is fishy."
@@ -63,7 +49,6 @@ EOF
 
 main(){
     start_job
-    check_master
     restart_kubelet
     finish_job
 }
