8831d8d5501c8d999d97c11c8b582e8a8b9e05aa
Kushagra Kulshreshtha <91978615+kkulshreshth@users.noreply.github.com>
2024-06-24 09:47:57 +0530

    Feature cluster autoscaler logs (#188)
    * Adding script files to collect and filter cluster autoscaler logs
    
    * Update script.sh
    
    Fixing: SC2086: Double quote to prevent globbing and word splitting.
    
    * Update metadata.yaml
    
    Removing unwanted api group
    
    * Update script.sh
    
    Replacing random sleep with check condition based on POD status.
    
    * Update script.sh
    
    Adding suggested arguments to make the script robust.
    
    * Update metadata.yaml
    
    Removing duplicate entry for list/get nodes
    
    * Update script.sh
    
    Replacing "ca" by "clusterautoscalers" and removing extra statement for getting pod name
    
    * Update metadata.yaml
    
    Adding mandatory "customerDataAccess: false" flag

---
+++ scripts/CEE/collect-verbose-ca-logs/README.md
@@ -0,0 +1,24 @@
+# Collect verbose logs from cluster-autoscaler pod
+
+## Description
+
+This script is responsible for collecting verbose logs (logVerbosity=6) from cluster-autoscaler pods
+
+## Steps It Takes
+
+1. The script checks the current verbosity of "default" cluster autoscaler and stores it in a variable
+2. Updates the value for logVerbosity to 6
+3. Collects the verbose logs for 6 minutes
+4. Filters the collected logs by node-names and displays the filtered logs.
+5. Reverts the value of logVerbosity.
+
+## Usage
+
+**ATTENTION** ⚠️ This script must only be used by the MCS (Managed Cloud Services) CRU (Crew Response Unit) team.
+- If you need this script to be used, contact a MCS CRU member.
+- The usage of this script is audited.
+
+```bash
+ocm backplane managedjob create CEE/collect-verbose-ca-logs"
+```
+
---
+++ scripts/CEE/collect-verbose-ca-logs/script.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+set -e
+set -o nounset
+set -o pipefail
+
+# Set variables
+GREEN="\033[32m"
+RESET="\033[0m"
+NAMESPACE="openshift-machine-api"
+CA_NAME="default"
+
+# Get current logVerbosity value
+current_log_verbosity=$(oc get clusterautoscalers $CA_NAME -o jsonpath='{.spec.logVerbosity}')
+
+# Check if current_log_verbosity is empty
+if [ -z "$current_log_verbosity" ]; then
+  echo "Failed to retrieve current logVerbosity value."
+  exit 1
+fi
+
+echo
+echo "CURRENT LOG VERBOSITY = $current_log_verbosity"
+echo
+
+# Update logVerbosity to 6
+oc patch clusterautoscalers $CA_NAME --type='json' -p='[{"op": "replace", "path": "/spec/logVerbosity", "value": 6}]'
+
+updated_log_verbosity=$(oc get clusterautoscalers $CA_NAME -o jsonpath='{.spec.logVerbosity}')
+echo
+echo "UPDATED LOG VERBOSITY = $updated_log_verbosity"
+echo
+
+# Wait for the update to take effect
+echo "Waiting for the log verbosity to update..."
+
+# Sleep for 5 seconds so that the new pod name gets reflected
+sleep 5
+
+CA_POD=$(oc get pods -n openshift-machine-api | grep cluster-autoscaler-default | awk '{print $1}')
+echo "POD Name: $CA_POD"
+
+while true; do
+  # Get the name and status of the cluster-autoscaler pod
+  POD_STATUS=$(oc get pod "$CA_POD" -n $NAMESPACE -o jsonpath='{.status.phase}')
+
+  # Check if the pod status is "Running"
+  if [ "$POD_STATUS" == "Running" ]; then
+    echo "The cluster-autoscaler pod is now 'Running'."
+    break
+  fi
+
+  # Print the current status and wait before rechecking
+  echo "Current status: $POD_STATUS. Waiting..."
+  sleep 5
+done
+
+# Collect logs for the next 6 minutes
+echo
+echo "Collecting logs for the next 6 minutes..."
+echo
+
+# Sleep for 6 minutes to let CA pod generate logs
+sleep 360
+
+echo "---------------------"
+echo "LOG COLLECTION: START"
+echo "---------------------"
+echo
+CA_LOGS=$(oc logs -n $NAMESPACE "$CA_POD" --since=6m)
+
+# Collect the list of nodes from the cluster
+node_names=$(oc get nodes -o jsonpath='{.items[*].metadata.name}')
+
+for node in $node_names; 
+do
+    echo -e "${GREEN}Searching for node: $node ${RESET}"
+    # Step 3: Display all the log lines where the node name is present
+    echo "$CA_LOGS" | grep "$node"
+    echo
+done
+
+echo
+echo "---------------------"
+echo "LOG COLLECTION: END"
+echo "---------------------"
+
+echo
+
+# Revert logVerbosity to previous value
+oc patch clusterautoscalers $CA_NAME --type='json' -p="[{'op': 'replace', 'path': '/spec/logVerbosity', 'value': $current_log_verbosity}]"
+
+# Wait for the update to take effect
+echo
+echo "Waiting for the log verbosity to be reverted back..."
+echo
+
+# Sleep for 5 seconds so that the new pod name gets reflected
+sleep 5
+
+CA_POD=$(oc get pods -n openshift-machine-api | grep cluster-autoscaler-default | awk '{print $1}')
+echo "POD Name: $CA_POD"
+
+while true; do
+  # Get the name and status of the cluster-autoscaler pod
+  POD_STATUS=$(oc get pod "$CA_POD" -n $NAMESPACE -o jsonpath='{.status.phase}')
+
+  # Check if the pod status is "Running"
+  if [ "$POD_STATUS" == "Running" ]; then
+    echo "The cluster-autoscaler pod is now 'Running'."
+    break
+  fi
+
+  # Print the current status and wait before rechecking
+  echo "Current status: $POD_STATUS. Waiting..."
+  sleep 5
+done
+
+current_log_verbosity=$(oc get clusterautoscalers $CA_NAME -o jsonpath='{.spec.logVerbosity}')
+echo "REVERTED LOG VERBOSITY = $current_log_verbosity"
+echo
